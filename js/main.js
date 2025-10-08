f36fd31b6eec5438f7adfda3dc75436a69165da7
Nima <nimaginobilisalami@gmail.com>
2025-10-08 18:36:04 +0200

    tilføjet den nye app.resevation.admin.js til main.js

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ import * as adminView from "./views/app.admin.js";
 import * as equipmentView from "./views/app.equipment.admin.js";
 import * as scheduleView from "./views/app.schedule.admin.js";
 import * as activityView from "./views/app.activity.admin.js";
-
+import * as reservationView from "./views/app.reservations.admin.js";
 
 console.log("SPA AdventureXP is running!");
 
@@ -18,7 +18,8 @@ const views = {
     admin: adminView,
     equipment: equipmentView,
     schedule: scheduleView,
-    activities: activityView
+    activities: activityView,
+    reservations: reservationView
 };
 
 // Funktion til at skifte mellem siderne
